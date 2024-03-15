1fb684823053dac6deaf5b30c49b832482b9ccf0
Andrew Parnell <aparnell@gmail.com>
2024-03-15 01:16:56 -0700

    Removed unnecessary comments, added listening to remotes for updates. Added validation that ensures no duplicate uuids.

--- src/platform.ts
+++ src/platform.ts
@@ -15,13 +15,14 @@ export class CeilingFanRemotePlatform implements DynamicPlatformPlugin {
   // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
   public readonly remotes: CeilingFanRemote[] = [];
+  public readonly initializedRemotes: string[] = [];
 
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    this.log.debug('Finished initializing ceiling fan platform');
+    this.log.debug('Initializing ceiling fan platform');
 
     // When this event is fired it means Homebridge has restored all cached accessories from disk.
     // Dynamic Platform plugins should only register new accessories after this event was fired,
@@ -30,7 +31,8 @@ export class CeilingFanRemotePlatform implements DynamicPlatformPlugin {
     this.api.on('didFinishLaunching', () => {
       this.log.debug('Executed didFinishLaunching callback');
       // run the method to discover / register your devices as accessories
-      this.discoverDevices();
+      this.initialize();
+      this.log.debug('Finished initializing ceiling fan platform');
     });
   }
 
@@ -45,68 +47,57 @@ export class CeilingFanRemotePlatform implements DynamicPlatformPlugin {
     this.accessories.push(accessory);
   }
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
-  discoverDevices() {
-
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
+  initialize() {
     this.log.debug('Discovering Devices');
 
-    // loop over the discovered devices and register each one if it has not already been registered
     if(this.config.remotes && Array.isArray(this.config.remotes)) {
+      // loop over the discovered devices and register each one if it has not already been registered
       for (const remoteConfig of this.config.remotes) {
-
-        // generate a unique id for the accessory this should be generated from
-        // something globally unique, but constant, for example, the device serial
-        // number or MAC address
         const uuid = this.api.hap.uuid.generate(remoteConfig.remote_id);
 
-        // see if an accessory with the same uuid has already been registered and restored from
-        // the cached devices we stored in the `configureAccessory` method above
-        const existingAccessoryIndex = this.accessories.findIndex(accessory => accessory.UUID === uuid);
+        if(this.initializedRemotes.indexOf(uuid)<0) {
+          this.initializedRemotes.push(uuid);
 
-        if (existingAccessoryIndex >= 0) {
-
-          const existingAccessory = this.accessories.splice(existingAccessoryIndex, 1)[0];
-          
-          // the accessory already exists
-          this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-          // Update the context to make sure the current settings are in the context
-          existingAccessory.context.config = remoteConfig;
-          this.api.updatePlatformAccessories([existingAccessory]);
-
-          // create the accessory handler for the restored accessory
-          // this is imported from `platformAccessory.ts`
-          this.remotes.push(new CeilingFanRemote(this, existingAccessory));
-        } else {
-          // the accessory does not yet exist, so we need to create it
-          this.log.info('Adding new accessory:', remoteConfig.name);
-
-          // create a new accessory
-          const accessory = new this.api.platformAccessory(remoteConfig.name, uuid);
-
-          // store a copy of the device object in the `accessory.context`
-          // the `context` property can be used to store any data about the accessory you may need
-          accessory.context.config = remoteConfig;
-
-          // create the accessory handler for the newly create accessory
-          // this is imported from `platformAccessory.ts`
-          this.remotes.push(new CeilingFanRemote(this, accessory));
-
-          // link the accessory to your platform
-          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+          // see if an accessory with the same uuid has already been registered and restored from
+          // the cached devices we stored in the `configureAccessory` method above
+          const existingAccessoryIndex = this.accessories.findIndex(accessory => accessory.UUID === uuid);
+  
+          if (existingAccessoryIndex >= 0) {
+            // the accessory already exists
+            const existingAccessory = this.accessories.splice(existingAccessoryIndex, 1)[0];
+  
+            this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+  
+            // Update the context to make sure the current settings are in the context
+            existingAccessory.context.config = remoteConfig;
+            this.api.updatePlatformAccessories([existingAccessory]);
+  
+            this.remotes.push(new CeilingFanRemote(this, existingAccessory));
+          } else {
+            // the accessory does not yet exist, so we need to create it
+            this.log.info('Adding new accessory:', remoteConfig.name);
+  
+            // create a new accessory
+            const accessory = new this.api.platformAccessory(remoteConfig.name, uuid);
+            accessory.context.config = remoteConfig;
+            this.remotes.push(new CeilingFanRemote(this, accessory));
+            this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+          }
+        }
+        else {
+          this.log.error(`Already initialized a remote with id ${remoteConfig.remote_id}. Remote ID must be unique.`);
         }
       }
 
+      //Add event listener to remotes for transmitting changes to mqtt
+      this.remotes.forEach(remote=>{
+        remote.addListener('update', props=>{
+          this.log.debug('Update received', props);
+        });
+      });
+
+      //Remove any cached remotes that aren't in the config anymore.
       this.accessories.forEach(existingAccessory=>{
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        // remove platform accessories when no longer present
         this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
         this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
       });
